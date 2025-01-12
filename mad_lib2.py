533297239c4727479f28b870c589741dd6a6a04a
Vinie254 <157572363+Vinie254@users.noreply.github.com>
2025-01-12 23:00:14 +0300

    Add files via upload

---
+++ mad_lib2.py
@@ -0,0 +1,14 @@
+adjective = input("Enter an adjective: ")
+type_of_bird = input("Enter the type of bird: ")
+room_in_a_house = input("Enter a room in a house: ")
+verb_past_tense = input("Enter a verb in past tense: ")
+verb = input("Enter a verb: ")
+relative_name = input("Enter a relative name: ")
+noun = input("Enter a noun: ")
+a_liquid = input("Enter a liquid: ")
+verb_ending_in_ing = input("Enter a verb ending in ing: ")
+part_of_the_body_Plural = input("Enter a part of the body in plural: ")
+plural_noun = input("Enter a plural noun: ")
+
+Madlibs_story = f"It was a {adjective}, cold November day. I woke up to the {adjective} smell of {type_of_bird} roasting in the  {room_in_a_house} downstairs.I {verb_past_tense} down the stairs to see if I could help {verb} the dinner.My mom said, 'See if {relative_name} needs a fresh {noun}.'So I carried a tray of glasses full of {a_liquid} into the {verb_ending_in_ing} room. When I got there, I couldn't believe my {part_of_the_body_Plural}! There {plural_noun} {verb_ending_in_ing} on the {noun} !"
+print(Madlibs_story)
