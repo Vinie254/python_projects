4bbb84dd079f2758a9018a5ea923cecebcb3b269
Vinie254 <157572363+Vinie254@users.noreply.github.com>
2025-06-14 21:54:58 +0300

    Create coin_game.py

---
+++ coin_game.py
@@ -0,0 +1,17 @@
+import random
+
+guess = ""
+while guess not in ("heads", "tails"):
+    print("Guess the coin toss! Enter heads or tails:")
+    guess = input()
+    toss = random.choice(["heads","tails"])
+    if toss == guess:
+        print("You got it!")
+    else:
+        print("Nope! Guess again!")
+    toss = random.choice(["heads","tails"])
+    guess = input()
+    if toss == guess:
+        print("You got it!")
+    else:
+        print("Nope. You are really bad at this game.")
